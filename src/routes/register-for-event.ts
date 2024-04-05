798e8e488344e452a44c7faea3921410a2ff2d2b
JoaoErick <jerick1415@outlook.com>
2024-04-05 10:48:15 -0300

    feat: add route to register for event

---
+++ src/routes/register-for-event.ts
@@ -0,0 +1,72 @@
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import z from "zod";
+import { prisma } from "../lib/prisma";
+import { BadRequest } from "./_errors/bad-requests";
+
+export async function registerForEvent(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .post('/events/:eventId/attendees', {
+      schema: {
+        summary: 'Register an attendee',
+        tags: ['attendees'],
+        body: z.object({
+          name: z.string().min(4),
+          email: z.string().email()
+        }),
+        params: z.object({
+          eventId: z.string().uuid()
+        }),
+        response: {
+          201: z.object({
+            attendeeId: z.number().int()
+          })
+        }
+      }
+    }, async (request, reply) => {
+      const { eventId } = request.params
+      const { name, email } = request.body
+
+      const attendeeFromEmail = await prisma.attendee.findUnique({
+        where: {
+          eventId_email: {
+            eventId,
+            email
+          }
+        }
+      })
+
+      if (attendeeFromEmail !== null) {
+        throw new BadRequest('This e-mail is already registeres for this event.')
+      }
+
+      // Executar as consultas no banco de dados em paralelo
+      const [event, amountOfAttendeesForEvent] = await Promise.all([
+        prisma.event.findUnique({
+          where: {
+            id: eventId
+          }
+        }),
+        prisma.attendee.count({
+          where: {
+            eventId
+          }
+        })
+      ])
+
+      if (event?.maximumAttendees && amountOfAttendeesForEvent >= event?.maximumAttendees) {
+        throw new BadRequest('The maximum number of attendees for this event has been reached.')
+      }
+
+      const attendee = await prisma.attendee.create({
+        data: {
+          name,
+          email,
+          eventId
+        }
+      })
+
+      return reply.status(201).send({ attendeeId: attendee.id })
+    })
+}
