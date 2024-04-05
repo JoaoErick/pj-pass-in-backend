af51355c14974265e0485ab6aa86957674975485
JoaoErick <jerick1415@outlook.com>
2024-04-05 10:46:32 -0300

    feat: add route to create event

---
+++ src/routes/create-event.ts
@@ -0,0 +1,54 @@
+import { serializerCompiler, validatorCompiler, ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod";
+import { generateSlug } from "../utils/generate-slug";
+import { prisma } from "../lib/prisma"
+import { FastifyInstance } from "fastify";
+import { BadRequest } from "./_errors/bad-requests";
+
+export async function createEvent(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .post('/events', {
+      schema: {
+        summary: 'Create an event',
+        tags: ['events'],
+        body: z.object({
+          title: z.string().min(4),
+          details: z.string().nullable(),
+          maximumAttendees: z.number().int().positive().nullable(),
+        }),
+        response: {
+          201: z.object({
+            eventId: z.string().uuid()
+          })
+        }
+      }
+    }, async (request, reply) => {
+
+      const data = request.body
+
+      const slug = generateSlug(data.title)
+
+      const eventWithSlug = await prisma.event.findUnique({
+          where: {
+              slug: slug
+          }
+      })
+
+      if (eventWithSlug !== null) {
+          throw new BadRequest('Another event with same title already exists.')
+      }
+
+      const event = await prisma.event.create({
+          data: {
+              title: data.title,
+              details: data.details,
+              maximumAttendees: data.maximumAttendees,
+              slug: slug
+          }
+      })
+
+      return reply.status(201).send({ eventId: event.id })
+    })
+}
+
